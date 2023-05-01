51f89bf62e963ee3b3098f379ec9ffc2a829eb12
Vitor Oliveira <chazan.uni@gmail.com>
2023-05-01 11:24:11 -0300

    Ajustando o jstl no maven

--- src/main/java/br/com/ufra/Servlet/ArmazenaDadosServlet.java
+++ src/main/java/br/com/ufra/Servlet/ArmazenaDadosServlet.java
@@ -4,12 +4,13 @@ package br.com.ufra.Servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/ArmazenaDadosServlet")
 public class ArmazenaDadosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
